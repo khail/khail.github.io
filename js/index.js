abd5800f5fd7d3db8b72f3c2da654386bd4cdf48
Khail <khailcs@yahoo.com>
2016-05-08 06:44:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,33 +1,47 @@
-window.smoothScroll = function( target )
-{ 	var scrollContainer = target;
-  	do
-  	{ 	//find scroll container
-    	scrollContainer = scrollContainer.parentNode;
-    	if( !scrollContainer ) return;
-    	scrollContainer.scrollTop += 1;
-  	} while( scrollContainer.scrollTop == 0 );
-    
-  	var targetY = 0;
-  	do
-  	{ 	//find the top of target relatively to the container
-    	if( target == scrollContainer ) break;
-      	targetY += target.offsetTop;
-  	} while( target = target.offsetParent );
-    
-  	scroll = function( c, a, b, i )
-  	{ 	if ( ++i > 30 ) return;
-    	c.scrollTop = a + ( b - a ) / 30 * i;
-    	setTimeout( function() { scroll( c, a, b, i ); }, 20 );
-  	}
-    
-  	// start scrolling
-  	scroll( scrollContainer, scrollContainer.scrollTop, targetY, 0 );
+window.smoothScroll = function (target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function (c, a, b, i) {
+        if (++i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function () {
+            scroll(c, a, b, i);
+        }, 20);
+    }
+
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
 
-$( "#btnMoocho" ).click( function() { smoothScroll( document.getElementById( "home-section" ) ); } );
-$( "#btnHome" ).click( function() { smoothScroll( document.getElementById( "home-section" ) ); } );
-$( "#btnAbout" ).click( function() { smoothScroll( document.getElementById( "about-section" ) ); } );
-$( "#btnProjects" ).click( function() { smoothScroll( document.getElementById( "projects-section" ) ); } );
-$( "#btnAwards" ).click( function() { smoothScroll( document.getElementById( "awards-section" ) ); } );
-$( "#btnSkillset" ).click( function() { smoothScroll( document.getElementById( "skillset-section" ) ); } );
-$( "#btnContact" ).click( function() { smoothScroll( document.getElementById( "links-section" ) ); } );
+$("#btnMoocho").click(function () {
+    smoothScroll(document.getElementById("home-section"));
+});
+$("#btnHome").click(function () {
+    smoothScroll(document.getElementById("home-section"));
+});
+$("#btnAbout").click(function () {
+    smoothScroll(document.getElementById("about-section"));
+});
+$("#btnProjects").click(function () {
+    smoothScroll(document.getElementById("projects-section"));
+});
+$("#btnAwards").click(function () {
+    smoothScroll(document.getElementById("awards-section"));
+});
+$("#btnSkillset").click(function () {
+    smoothScroll(document.getElementById("skillset-section"));
+});
+$("#btnContact").click(function () {
+    smoothScroll(document.getElementById("links-section"));
+});
